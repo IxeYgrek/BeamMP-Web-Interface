a0a1be3fca6734e07729f010450bfb0e21920d3c
IxeYgrek <ixeygrek@protonmail.ch>
2024-06-09 15:02:05 +0200

    Update changemap_script.sh - Multiple instances
    Added management of the database table name as a configurable variable in the configuration file, in order to manage multiple instances.

--- beamng/scripts/changemap_script.sh
+++ beamng/scripts/changemap_script.sh
@@ -13,6 +13,7 @@ config_file="$(dirname "$0")/../config"
 username=$(grep '^username=' "$config_file" | cut -d "=" -f2 | tr -d '[:space:]')
 password=$(grep '^password=' "$config_file" | cut -d "=" -f2 | tr -d '[:space:]')
 dbname=$(grep '^dbname=' "$config_file" | cut -d "=" -f2 | tr -d '[:space:]')
+tablename=$(grep '^tablename=' "$config_file" | cut -d "=" -f2 | tr -d '[:space:]')
 
 # Variables emplacement des dossiers
 
@@ -28,31 +29,31 @@ beammpservice=$(grep '^beammpservice=' "$config_file" | cut -d "=" -f2 | tr -d '
 
 # Variables ID Map Active actuelle
 
-id_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT id_beamng FROM beamng_mod WHERE map_active = 1;")
+id_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT id_beamng FROM $tablename WHERE map_active = 1;")
 
 # Variable map actuelle officielle ou pas
 
-official_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT map_officielle FROM beamng_mod WHERE map_active = 1;")
+official_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT map_officielle FROM $tablename WHERE map_active = 1;")
 
 # Variable nouvelle map officielle ou pas
 
-official_new_map=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT map_officielle FROM beamng_mod WHERE id_beamng = '$1';")
+official_new_map=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT map_officielle FROM $tablename WHERE id_beamng = '$1';")
 
 # Variable chemin du zip de la map active
 
-chemin_zip_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM beamng_mod WHERE map_active = 1;")
+chemin_zip_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM $tablename WHERE map_active = 1;")
 
 # Variable chemin du zip de la nouvelle map
 
-chemin_zip_new_map=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM beamng_mod WHERE id_beamng = '$1';")
+chemin_zip_new_map=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM $tablename WHERE id_beamng = '$1';")
 
 # Variable nom du zip de la map active
 
-nom_zip_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM beamng_mod WHERE map_active = 1;" | cut -d "/" -f8)
+nom_zip_map_active=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM $tablename WHERE map_active = 1;" | cut -d "/" -f8)
 
 # Variable nom du zip de la nouvelle map
 
-nom_zip_new_map=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM beamng_mod WHERE id_beamng = '$1';" | cut -d "/" -f8)
+nom_zip_new_map=$(mysql -D $dbname -u$username --password="$password" -N -e "SELECT chemin FROM $tablename WHERE id_beamng = '$1';" | cut -d "/" -f8)
 
 
 
@@ -74,22 +75,22 @@ fi
 # On met à jour dans la base de données le chemin de la map active pour que ça corresponde au chemin des map inactive
 
 if [ "$official_map_active" -eq 0 ]; then
-    mysql -D $dbname -u$username --password="$password" -e "UPDATE gaming.beamng_mod SET chemin = REPLACE(chemin, 'Client', 'inactive_map') WHERE id_beamng = '$id_map_active';"
+    mysql -D $dbname -u$username --password="$password" -e "UPDATE ${dbname}.${tablename} SET chemin = REPLACE(chemin, 'Client', 'inactive_map') WHERE id_beamng = '$id_map_active';"
 fi
 
 # On met à jour dans la base de données le chemin de la nouvelle map pour que ça corresponde au chemin de la map active
 
 if [ "$official_new_map" -eq 0 ]; then
-    mysql -D $dbname -u$username --password="$password" -e "UPDATE gaming.beamng_mod SET chemin = REPLACE(chemin, 'inactive_map', 'Client') WHERE id_beamng = '$1';"
+    mysql -D $dbname -u$username --password="$password" -e "UPDATE ${dbname}.${tablename} SET chemin = REPLACE(chemin, 'inactive_map', 'Client') WHERE id_beamng = '$1';"
 fi
 
 # Mise à 0 de la valeur map_active pour la map active
 
-mysql -D $dbname -u$username --password="$password" -e "UPDATE beamng_mod SET map_active = 0 WHERE id_beamng = '$id_map_active';"
+mysql -D $dbname -u$username --password="$password" -e "UPDATE $tablename SET map_active = 0 WHERE id_beamng = '$id_map_active';"
 
 # Valeur map_active à 1 pour la map choisi
 
-mysql -D $dbname -u$username --password="$password" -e "UPDATE beamng_mod SET map_active = 1 WHERE id_beamng = '$1';"
+mysql -D $dbname -u$username --password="$password" -e "UPDATE $tablename SET map_active = 1 WHERE id_beamng = '$1';"
 
 # On remplace la map actuelle par la nouvelle MAP dans le fichier de conf du serveur
 
